55bdfaa2b8f17fb5d20cc0788f4005c5adc57ce5
Lisbon <lisbon@dublin.com>
2023-06-01 16:45:23 +0300

    Kintamųjų inicijavimas

--- js/01-console.js
+++ js/01-console.js
@@ -1,1 +1,60 @@
 console.clear();
+
+// Kintamųjų inicijavimas
+
+/*
+Sukurti 3 kintamuosius su skaičiaus tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+pear = 1;
+apple = 2;
+banana = 7;
+
+console.log(pear);
+console.log(apple);
+console.log(banana);
+
+/*
+Sukurti 3 kintamuosius su teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+city = 'Vilnius';
+country = 'Lithuania';
+continent = 'Europe';
+
+console.log(city);
+console.log(country);
+console.log(continent);
+
+/*
+Sukurti 3 sąrašo tipo kintamuosius su penkiomis skaičių tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+age1 = 15;
+age2 = 20;
+age3 = 2;
+
+// ??
+
+console.log();
+console.log();
+console.log();
+
+/*
+Sukurti 3 sąrašo tipo kintamuosius su penkiomis teksto tipo reikšmėmis
+Po kiekvieno jų inicijavimo, išvesti į console
+*/
+
+flag = ('white');
+nationality = 'Lithuanian';
+race = 'Caucasian';
+
+// ??
+
+console.log();
+console.log();
+console.log();
+
