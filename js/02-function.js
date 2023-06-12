68c21bdc7842b6a42845b8955ab63aca4dcbc3be
Lisbon <lisbon@dublin.com>
2023-06-12 09:54:09 +0300

    Exc. function "number length"

--- js/02-function.js
+++ js/02-function.js
@@ -45,6 +45,8 @@ console.log( daugyba( skaicius1, skaicius3 ) );
 rezultatas: teisingos reikšmės;
 */
 
+/* Neteisingas sprendimas!!! 
+
 console.log('.............');
 
 function daugyba(pirmasSkaicius, antrasSkaicius) {
@@ -58,6 +60,9 @@ const skaicius3 = 8;
 console.log( daugyba( skaicius1, skaicius2 ) );
 console.log( daugyba( skaicius3, skaicius2 ) );
 console.log( daugyba( skaicius1, skaicius3 ) );
+*/
+
+
 
 
 /*
@@ -80,7 +85,62 @@ console.log( skaitmenuKiekisSkaiciuje( “asd” ) );
 rezultatas: “Pateikta netinkamo tipo reikšmė.”
 console.log( skaitmenuKiekisSkaiciuje( NaN ) );
 rezultatas: “Pateikta netinkamo tipo reikšmė.”
+*/
 
+console.log(); // paskaitos sprendimas:
+
+function numberLength(number) {
+    if (typeof number !== 'number') {
+        return 'ERROR: duok skaiciu.';
+    }
+    if (isNaN(number)) {
+        return 'ERROR: duok normalu skaiciu, o ne NaN.';
+    }
+    if (number === Infinity || number === -Infinity) {
+        return 'ERROR: duok normalu skaiciu, o ne begalybe.';
+    }
+
+    const numberAsString = '' + number;
+    let size = numberAsString.length;
+
+    // tikriname ar yra minusas?
+    if (number < 0) {
+        size--;
+    }
+
+    // tikriname ar skaicius yra desimtainis?
+    if (number % 1 !== 0) {
+        size--;
+    }
+
+    return size;
+}
+
+// Privalomi testai:
+
+console.log(numberLength(true));
+console.log(numberLength(false));
+console.log(numberLength('asd'));
+console.log(numberLength(NaN));
+console.log(numberLength(Infinity));
+console.log(numberLength(-Infinity));
+console.log(numberLength(undefined));
+console.log(numberLength());
+console.log(numberLength([]));
+console.log(numberLength(numberLength));
+console.log('--------------------------------');
+console.log(numberLength(5), '->', 1);
+console.log(numberLength(781), '->', 3);
+console.log(numberLength(37060123456), '->', 11);
+console.log(numberLength(-5), '->', 1);
+console.log(numberLength(-781), '->', 3);
+console.log(numberLength(-37060123456), '->', 11);
+console.log(numberLength(3.14), '->', 3);
+console.log(numberLength(2.727), '->', 4);
+console.log(numberLength(-3.14), '->', 3);
+console.log(numberLength(-2.727), '->', 4);
+
+/*
 Funkcija pavadinimu “didziausiasSkaiciusSarase”:
 priima vieną kintamąjį
 jei perduotas kintamasis nėra sąrašo tipo, tai išveda pranešimą “Pateikta netinkamo tipo reikšmė.”
